996224a5c9cbf21bc40766374ebd315b4d0610b3
MathewNdekudugu <mdekudugu2015@gmail.com>
2019-10-23 21:57:40 +0000

    refactor front end files

--- app.js
+++ app.js
@@ -2,33 +2,48 @@ const express=require('express')
 const http=require('http')
 const bodyParser=require('body-parser')
 const path=require('path')
+const exphbs=require('express-handlebars')
 
 // instance of the app
 const app =express()
 
 
-// Middlewares
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
-// view engine setup
-app.set('view', path.join(__dirname, 'view'));
-app.set('view engine', 'hbs');
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json());
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('working')
+
+
+app.get('/index', (req, res) => {
+    res.render('index',{layout:false});
+    
 });
 
-
-app.get('/home', (req, res) => {
-    res.render(index);
+app.get('/blank', (req, res) => {
+    res.render('blank',{layout:false});
+    
+});
+app.get('/checkout', (req, res) => {
+    res.render('checkout',{layout:false});
+    
+});
+app.get('/product', (req, res) => {
+    res.render('product',{layout:false});
     
 });
 
 
+app.get('/store', (req, res) => {
+    res.render('store',{layout:false});
+    
+});
 const server = http.createServer(app)
 
 server.listen(process.env.PORT || 8000, (req,res)=>{
